4aad0b4970609e8852727e47fbd2dbaae60e84ae
albinzebi <albinzebi@outlook.com>
2025-04-10 15:19:02 +0200

    created class Forecast

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -3,6 +3,7 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
 
 const updateUI = (data) => {
 
@@ -33,14 +34,6 @@ const updateUI = (data) => {
     }
 };
 
-
-const updateCity = async (city) => {
-    const cityDets = await getCity(city);
-    const weather = await getWeather(cityDets.Key);
-
-    return { cityDets, weather };
-};
-
 cityForm.addEventListener('submit', e => {
     // prevent default action
     e.preventDefault();
@@ -54,14 +47,14 @@ cityForm.addEventListener('submit', e => {
 
 
     // update the ui with new city
-    updateCity(city)
+    forecast.updateCity(city)
         .then(data => updateUI(data))
         .catch(err => console.log(err));
 });
 
 // if local storage is not empty get that value from local storage
 if (localStorage.getItem('city')) {
-    updateCity(localStorage.getItem('city'))
+    forecast.updateCity(localStorage.getItem('city'))
         .then(data => updateUI(data))
         .catch(err => console(err));
 }
